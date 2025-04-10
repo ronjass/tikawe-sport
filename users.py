85f9f6c7e4b1a2a4385aa2abfe0c803a5e6faa7a
Ronja <ronja.sonninen@helsinki.fi>
2025-04-10 09:04:50 +0300

    Improve the style of the user page

--- users.py
+++ users.py
@@ -7,7 +7,10 @@ def get_user(user_id):
     return result[0] if result else None
 
 def get_sports(user_id):
-    sql = "SELECT id, sport, duration, distance FROM sports WHERE user_id = ? ORDER BY id DESC"
+    sql = """SELECT id, sport, duration, distance, sent_at 
+             FROM sports 
+             WHERE user_id = ? 
+             ORDER BY id DESC"""
     return db.query(sql, [user_id])
 
 def create_user(username, password):
